80765c991693343a7780413375a4671978fc45a1
ihr@edilion.com <hrynchyshynKI33>
2019-07-20 14:18:16 +0300

    Add material login ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,45 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LoginService} from '../services/login.service';
 import {Credentials} from '../models/user';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  username: string;
-  password: string;
+  // username: string;
+  // password: string;
 
-  constructor(private loginService: LoginService) {
+  public form: FormGroup;
+
+  constructor(
+    private loginService: LoginService,
+    private fb: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.getLoginForm();
   }
 
-  login() {
-    const cred = new Credentials(this.username, this.password);
-    this.loginService.login(cred);
+  public login(): void {
+    this.loginService.login(<Credentials>this.form.value);
+  }
+
+  private getLoginForm(): FormGroup {
+    return this.fb.group({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl(''),
+    })
   }
 
   isLoggedIn(): boolean {
     return this.loginService.isLoggedIn();
+  }
+
+  logOff(){
+    this.loginService.logoff();
   }
 }
