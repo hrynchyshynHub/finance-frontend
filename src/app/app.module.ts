8d428bbb0a194eedeab63bec3d778f76826e4930
ihr@edilion.com <hrynchyshynKI33>
2019-07-20 14:23:48 +0300

    Update material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {InputsModule} from 'angular-bootstrap-md';
-import { ChartsModule } from 'ng2-charts';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
 import {AppComponent} from './app.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {NavigationBarComponent} from './navigation-bar/navigation-bar.component';
-import {NoteManagmentComponentComponent} from './note-managment-component/note-managment-component.component';
 import {NotificationComponent} from './notification/notification.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {FinanceManagmentComponentComponent} from './finance-managment-component/finance-managment-component.component';
-import {CurrencyComponent} from './currency/currency.component';
 import {LoginComponent} from './login/login.component';
 import {AuthGuard} from './_guards/auth.guard';
-import {FinanceInterceptor} from './_shared/finance.interceptor';
+import {AuthInterceptor} from './_shared/auth-intereceptor.service';
 import {LoginService} from './services/login.service';
-import { BudgetsComponent } from './budgets/budgets.component';
-import {TransactionComponent} from "./transaction/transaction.component";
-import { BudgetItemComponent } from './budget-item/budget-item.component';
-import { NewsManagmentComponent } from './news-managment/news-managment.component';
-import { StatisticsComponent } from './statistics/statistics.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+import {MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule} from '@angular/material';
 
 
 @NgModule({
@@ -32,29 +23,33 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     AppComponent,
     PageNotFoundComponent,
     NavigationBarComponent,
-    NoteManagmentComponentComponent,
     NotificationComponent,
-    FinanceManagmentComponentComponent,
-    CurrencyComponent,
     LoginComponent,
-    BudgetsComponent,
-    TransactionComponent,
-    BudgetItemComponent,
-    NewsManagmentComponent,
-    StatisticsComponent
   ],
   imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MaterialModule,
     NgbModule,
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     InputsModule,
     AppRoutingModule,
-    ChartsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+  ],
+  exports:[
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MaterialModule,
   ],
   providers: [LoginService, AuthGuard,
-    {provide: HTTP_INTERCEPTORS, useClass: FinanceInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
